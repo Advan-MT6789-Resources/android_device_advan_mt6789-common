a94375746f33a0e1cb5b531110bc6e169080ce09
Giovanni Ricca <giovanniricca@proton.me>
2024-11-14 11:58:48 +0100

    mt6789-common: Migrate to py extract utils
    Change-Id: Id8aa8df49451f3ed293542c63de954fbe4806a92

---
+++ extract-files.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc': blob_fixup()
+        .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
+    'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
+    'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .add_needed('libshim_sensors.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    (
+        'vendor/bin/hw/android.hardware.gnss-service.mediatek',
+        'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    (
+        'vendor/lib64/libwvhidl.so',
+        'vendor/lib64/mediadrm/libwvdrmengine.so',
+    ): blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
+    (
+        'vendor/bin/mnld',
+        'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so',
+        'vendor/lib64/mt6789/libaalservice.so',
+    ): blob_fixup()
+        .add_needed('libshim_sensors.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'mt6789-common',
+    'advan',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
