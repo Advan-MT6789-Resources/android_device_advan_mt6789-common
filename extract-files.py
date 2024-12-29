868608b797994a8c76171d0cb23b1dfe47f0c853
nift4 <nift4@protonmail.com>
2024-12-29 16:16:47 +0100

    mt6789-common: Add missing dependencies to offending libraries
    * Some libraries depend on symbols from other libraries that aren't
      defined as NEEDED. Since checkelf will throw an error
      in this case, use patchelf to add the missing libraries to
      NEEDED.
    
    Change-Id: I30c7fb2237420dedc8e4966f9c57cf07d099ffa3

--- extract-files.py
+++ extract-files.py
@@ -45,6 +45,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
         .add_needed('libshim_sensors.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/mt6789/libmnl.so' : blob_fixup()
+        .add_needed('libcutils.so'),
     (
         'vendor/bin/hw/android.hardware.gnss-service.mediatek',
         'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so',
