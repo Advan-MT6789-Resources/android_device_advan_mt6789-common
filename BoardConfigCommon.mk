223b1da3ebc60b6c2c75a0f3f6a746ecebd293c0
dodyirawan85 <dev.irawans@gmail.com>
2025-11-23 23:54:10 +0700

    mt6789-common: Initial sepolicy based on T812_B15_V4.0L.6_20250724
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Vendor Security Patch
 VENDOR_SECURITY_PATCH := 2025-06-05
