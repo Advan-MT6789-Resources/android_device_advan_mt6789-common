6ec175987844fe561370c821df858bbed55431ce
dodyirawan85 <dev.irawans@gmail.com>
2025-11-25 00:01:12 +0700

    Revert "mt6789-common: Drop system IMS stack for now"
    This reverts commit 94ac6ec83af9ff07e9d52e28a0d16b6c1d8776a5.

--- common.mk
+++ common.mk
@@ -167,6 +167,15 @@ PRODUCT_PACKAGES += \
     libutilscallstack.vendor
 
 # IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
