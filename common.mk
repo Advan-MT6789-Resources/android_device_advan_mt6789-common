08b3dc74dbf8f75d6b2b3158c48b010851263e46
dodyirawan85 <dev.irawans@gmail.com>
2026-01-02 06:49:05 +0700

    Revert "mt6789-common: Add keylayout file to map double tap event to wake"
    * Our device don't need this
    
    This reverts commit d52a9e3e887153f5e1fcb80bf80053c7e091b833.
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -156,10 +156,6 @@ PRODUCT_PACKAGES += \
     init.insmod.mt8781.cfg \
     init.mt8781.rc
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
-
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
