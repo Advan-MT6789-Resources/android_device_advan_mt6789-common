678e906ea728f05cb0bc6434c10e69ede479a2bc
dodyirawan85 <dev.irawans@gmail.com>
2026-01-22 22:17:53 +0700

    mt6789-common: Enable support for mtk ged kpi libgui
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -90,6 +90,8 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Display
+$(call soong_config_set_bool,libgui,support_mtk_ged_kpi,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek-mali
